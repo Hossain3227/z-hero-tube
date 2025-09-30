ea04e26124f8dc5fe166d76d0c9bbd96edb59fc5
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-30 13:00:42 +0600

    added search functionality

--- script/index.js
+++ script/index.js
@@ -81,8 +81,8 @@ const displayVideoDetails= (video)=> {
 }
 
 
-function loadVideos() {
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+function loadVideos(searchText = "") {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then((res) => res.json())
     .then((data) =>{ 
       removeActiveClass();
@@ -160,7 +160,7 @@ const displayVideos = (videos) => {
             <p class="text-sm text-gray-400 flex gap-1">
             ${video.authors[0].profile_name}
 
-              <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="">
+            ${video.authors[0].verified == true ? ` <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="">`:``}
             </p>
             <p class="text-sm text-gray-400">
               ${video.others.views}
@@ -175,6 +175,12 @@ const displayVideos = (videos) => {
     videoContainer.append(videoCard);
   });
 };
+
+document.getElementById('search-input').addEventListener('keyup',(e)=> {
+  const input = e.target.value;
+  loadVideos(input);
+  
+})
 
 loadCategoris();
 // loadVideos();
